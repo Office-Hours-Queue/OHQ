60b35b5ef6622b026936a106ba360e61f514f020
Kevin Zheng <sunday0303@gmail.com>
2016-07-19 22:06:10 -0400

    Add config entry for Google APIs

--- config/dev.js
+++ config/dev.js
@@ -1,3 +1,10 @@
 var config = {};
 
+// Get/set these values from the Google APIs console
+config.GOOGLE_OAUTH2_CONFIG = {
+  clientID: 'myclientid.apps.googleusercontent.com',
+  clientSecret: 'supersecret',
+  callbackURL: 'http://localhost:3000/success'
+};
+
 module.exports = config;
