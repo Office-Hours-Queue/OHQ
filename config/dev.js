064eacc9e47155dcf0318f74ff1c2ff06acc0c64
Kevin Zheng <sunday0303@gmail.com>
2016-07-20 01:05:52 -0400

    Add knex connection configuration

--- config/dev.js
+++ config/dev.js
@@ -7,4 +7,10 @@ config.GOOGLE_OAUTH2_CONFIG = {
   callbackURL: 'http://localhost:3000/success'
 };
 
+// Connection to database
+config.KNEX = {
+  client: 'postgresql',
+  connection: 'postgres://someuser:somepassword@somehost:381/sometable'
+};
+
 module.exports = config;
