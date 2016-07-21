e40f419080800a873575e65e6b7581525dce5c2a
Kevin Zheng <sunday0303@gmail.com>
2016-07-20 23:06:31 -0400

    Persist sessions into database

--- app.js
+++ app.js
@@ -5,14 +5,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var dbSessionStore = require('connect-session-knex')(session);
 var auth = require('./auth');
+var db = require('./db');
 
 // import the routes we've written
 var index = require('./routes/index');
 var queue = require('./routes/queue');
 
 var app = express();
-
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,7 +24,14 @@ app.use(cookieParser());
 app.use(session({
   secret: 'supersecret',
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60  // 1 hour
+  },
+  store: new dbSessionStore({
+    knex: db,
+    tablename: 'sessions'
+  })
 }));
 app.use(auth.passport.initialize());
 app.use(auth.passport.session());
