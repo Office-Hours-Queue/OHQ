818dfdf254bc8a271d30539dff291922a88df6a4
Kevin Zheng <sunday0303@gmail.com>
2016-07-26 20:49:20 -0400

    Add realtime postgres table events

---
+++ db-events.js
@@ -0,0 +1,27 @@
+var config = require('./config');
+
+var EventEmitter = require('events').EventEmitter;
+var Client = require('pg').Client;
+
+// we need a dedicated connection to the db to listen for notifications
+var client = new Client(config.KNEX.connection);
+client.connect();
+
+// we'll unpack and forward notifications via these emitters
+var emitters = {};
+emitters.questions = new EventEmitter();
+emitters.queue_meta = new EventEmitter();
+
+// pg sends us events when certain tables get updated
+client.on('notification', function(msg) {
+  var payload = JSON.parse(msg.payload);
+  var table = payload.table;
+  var action = payload.action.toLowerCase();
+  var id = payload.id;
+  emitters[table].emit(action, id);
+});
+
+// subscribe to the update stream
+client.query('LISTEN table_update');
+
+module.exports = emitters;
