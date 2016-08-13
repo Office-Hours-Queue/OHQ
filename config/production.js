1fdef43ff548ad1974ff807251f17122e3de75e8
Edward Dryer <edryer@andrew.cmu.edu>
2016-08-12 22:32:06 -0400

    added production mode

---
+++ config/production.js
@@ -0,0 +1,16 @@
+var config = {};
+
+// Get/set these values from the Google APIs console
+config.GOOGLE_OAUTH2_CONFIG = {
+  clientID: '458287048062-72ov34gb94lep95onf4ret5lalbkpie4.apps.googleusercontent.com',
+  clientSecret: 'RpGBRq_ol6BBNLEpAYjYCImk',
+  callbackURL: 'http://edwarddryer.com:5000/login/success'
+};
+
+// Connection to database
+config.KNEX = {
+  client: 'postgresql',
+  connection: 'postgres://queue:supersecret@127.0.0.1:5432/queue'
+};
+
+module.exports = config;
