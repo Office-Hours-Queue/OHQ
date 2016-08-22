c6eb50532f8ad53417726ee3cfa8a5dc2034ea76
edwdryer <edryer@andrew.cmu.edu>
2016-08-21 21:30:54 -0400

    fix #83 overlay issue

--- app/static/js/controller/admin.js
+++ app/static/js/controller/admin.js
@@ -2,6 +2,7 @@ var admin_ctl = ["$scope","$rootScope","$db","$http",function($scope,$rootScope,
 	$rootScope.$db = $db;
 	$rootScope.check_login();
 	$rootScope.current_page = "admin";
+	$("#sidenav-overlay").remove();
 	$scope.add_andrew_id = function () {
     $http.post("/api/validusers/add/student", [{ andrew_id: $("#new_andrew_id").val() }]);
 	}
