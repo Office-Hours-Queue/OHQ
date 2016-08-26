fd7772b3b527c09e31282383c307a4beef3a4899
Edward Dryer <edryer@andrew.cmu.edu>
2016-08-26 13:49:20 -0400

    tested and fixed docker exec migrate script

--- deploy_configs/migrate_and_seed.sh
+++ deploy_configs/migrate_and_seed.sh
@@ -1,2 +1,2 @@
-docker exec 112_queue_nodeapp npm migrate 
-docker exec 112_queue_nodeapp npm seed 
+docker exec 112_queue_nodeapp bash -c 'npm run-script migrate' 
+docker exec 112_queue_nodeapp bash -c 'npm run-script seed'
