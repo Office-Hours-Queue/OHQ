87252e6bd7bee5e2b9bab120247c2aa8bd31cfe0
Edward Dryer <edryer@andrew.cmu.edu>
2016-08-27 10:56:32 -0400

    change mv to cp in backup, fix path bug in service

--- backup/install.sh
+++ backup/install.sh
@@ -1,5 +1,5 @@
-mv 112_queue_db_backup.service /etc/systemd/system/112_queue_db_backup.service
-mv 112_queue_db_backup.timer /etc/systemd/system/112_queue_db_backup.timer
+cp 112_queue_db_backup.service /etc/systemd/system/112_queue_db_backup.service
+cp 112_queue_db_backup.timer /etc/systemd/system/112_queue_db_backup.timer
 systemctl enable 112_queue_db_backup.timer
 systemctl start 112_queue_db_backup.timer
 
