7ad3e75093cab0fb3c93beca5dd2dae73a66ca51
Edward Dryer <edryer@andrew.cmu.edu>
2016-09-04 15:10:44 -0400

    add data.csv to redirect

--- backup/get_csv.sh
+++ backup/get_csv.sh
@@ -1,1 +1,1 @@
-docker exec 112_queue_postgres psql -U queue queue -c 'copy (select * from questions) to stdout with csv;'  > "/home/queue-dev/15-112-Queue/backup/"
+docker exec 112_queue_postgres psql -U queue queue -c 'copy (select * from questions) to stdout with csv;'  > "/home/queue-dev/15-112-Queue/backup/data.csv"
