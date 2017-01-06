8d86850e49ed70fc5ca55b5e4aa881f69698e0ab
Kevin Zheng <sunday0303@gmail.com>
2017-01-06 02:24:06 -0600

    Add frontend functionality for TA time spent

--- app/static/js/controller/stats.js
+++ app/static/js/controller/stats.js
@@ -1,27 +1,37 @@
-var stats_ctl = ['$scope', '$rootScope', '$db', function($scope, $rootScope, $db) {
+var stats_ctl = ['$scope', '$rootScope', '$db', '$http', function($scope, $rootScope, $db, $http) {
 
 	$rootScope.$db = $db;
 	$rootScope.check_login();
 
-  var socket = io('/stats/questioncount');
+  // handle the question count stats
+  (function() {
+    var socket = io('/stats/questioncount');
 
-  $scope.ca_counts = [];
-  $scope.questions_answered = 0;
+    $scope.ca_counts = [];
+    $scope.questions_answered = 0;
 
-  var ca_counts = {};
+    var ca_counts = {};
 
-  socket.on('question_count', function(user) {
-    console.log('question_count', user);
-    ca_counts[user.user_id] = user;
-    $scope.$apply(function() {
-      $scope.ca_counts = [];
-      $scope.questions_answered = 0;
-      var userids = Object.keys(ca_counts);
-      for (var i = 0; i < userids.length; i++) {
-        $scope.ca_counts.push(ca_counts[userids[i]]);
-        $scope.questions_answered += ca_counts[userids[i]].question_count;
-      }
+    socket.on('question_count', function(user) {
+      ca_counts[user.user_id] = user;
+      $scope.$apply(function() {
+        $scope.ca_counts = [];
+        $scope.questions_answered = 0;
+        var userids = Object.keys(ca_counts);
+        for (var i = 0; i < userids.length; i++) {
+          $scope.ca_counts.push(ca_counts[userids[i]]);
+          $scope.questions_answered += ca_counts[userids[i]].question_count;
+        }
+      });
     });
-  });
+  })();
+
+  // handle the hours stats
+  (function() {
+    $scope.time_spent_answering = 0;
+    $http.get('/api/stats/timespent/answering').then(function(resp) {
+      $scope.time_spent_answering = resp.data.seconds;
+    });
+  })();
 
 }];
