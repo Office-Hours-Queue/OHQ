4483be6eeabe27836f572a4843b95fea3090300c
Kevin Zheng <sunday0303@gmail.com>
2017-01-09 01:30:15 -0600

    Make student ask question dropdowns work reliably

--- app/static/js/controller/student.js
+++ app/static/js/controller/student.js
@@ -1,120 +1,119 @@
 var student_ctl = ["$scope","$rootScope","$db",function($scope,$rootScope,$db) {
-	$rootScope.$db = $db;
-	$rootScope.current_page = "student";
-	$scope.name = "student";
+  $rootScope.$db = $db;
+  $rootScope.current_page = "student";
+  $scope.name = "student";
 
-	$rootScope.check_login();
+  $rootScope.check_login();
 
-	var example_questions = [
-	'Help me go over the code tracing from quiz 1.',
-	'What does \'type affects semantics\' mean?',
-	'Why do I need to use almostEquals?',
-	'Should I use almostEquals in this function?',
-	];
+  var example_questions = [
+  'Help me go over the code tracing from quiz 1.',
+  'What does \'type affects semantics\' mean?',
+  'Why do I need to use almostEquals?',
+  'Should I use almostEquals in this function?',
+  ];
 
 
-            $scope.clicked_faq = function () {
-                ga('send', 'event','FAQ','student clicked',$rootScope.user["andrew_id"])
-            };
+  $scope.clicked_faq = function () {
+    ga('send', 'event','FAQ','student clicked',$rootScope.user["andrew_id"])
+  };
 
-            $scope.clicked_debugging_tips = function () {
-                ga('send', 'event','Debugging Tips','student clicked',$rootScope.user["andrew_id"])
-            };
+  $scope.clicked_debugging_tips = function () {
+    ga('send', 'event','Debugging Tips','student clicked',$rootScope.user["andrew_id"])
+  };
 
-	function set_random_question() {
-	var random_idx = Math.floor(Math.random()*example_questions.length);
-	$scope.example_question = example_questions[random_idx];
-	}
+  function set_random_question() {
+  var random_idx = Math.floor(Math.random()*example_questions.length);
+  $scope.example_question = example_questions[random_idx];
+  }
 
-	set_random_question();
+  set_random_question();
 
-	$scope.selected = {
-	"topic": "",
-	"location":"",
-	"help_text":""
-	};
+  $scope.toggle_freeze = function() {
+    if ($db.is_frozen()) {
+      $db.unfreeze_question();
+    } else if ($db.can_freeze()) {
+      $db.freeze_question();
+    }
+  }
 
-	$scope.toggle_freeze = function() {
-		if ($db.is_frozen()) {
-			$db.unfreeze_question() 
-		} else {
-			if ($db.can_freeze()) {
-				$db.freeze_question()
-			}
-		}
-	}
+  // the current data in the new question form
+  $scope.new_selected = {
+    topic: '',
+    location: '',
+    help_text: ''
+  };
 
-  $scope.$watch(function() {
-    return $db.model.questions[0];
+  // the current data in the edit question form
+  $scope.edit_selected = {
+    topic: '',
+    location: '',
+    help_text: ''
+  };
+
+  // when the current question changes, update the edit form model and save
+  // the location for the next question
+  $scope.$watchCollection(function() {
+    return $db.model.questions;
   }, function() {
     if ($db.model.questions.length > 0) {
-      $scope.selected = {
-        "topic": $db.model.questions[0].topic_id.toString(),
-        "location": $db.model.questions[0].location_id.toString(),
-        "help_text": $db.model.questions[0].help_text
-      };
-    } else if (
-        typeof $db.model.topics !== 'undefined' &&
-        typeof $db.model.locations !== 'undefined' &&
-        $db.model.topics.length > 0 &&
-        $db.model.locations.length > 0) {
-      $scope.selected = {
-        "topic": $db.model.topics[0].id,
-        "location": $db.model.locations[0].id,
-        "help_text": ""
-      };
-    } else {
-      $scope.selected = {
-        "topic": "",
-        "location":"",
-        "help_text":""
-      };
+      $scope.edit_selected.topic = $db.model.questions[0].topic_id.toString();
+      $scope.edit_selected.location = $db.model.questions[0].location_id.toString();
+      $scope.edit_selected.help_text = $db.model.questions[0].help_text;
+      $scope.new_selected.location = $db.model.questions[0].location_id.toString();
     }
   });
 
-	var unbind_topic_watch = $scope.$watch(function () {
-		return $db.model['topics'].length;	
-	},function() {
-		if ($db.model['topics'].length > 0) {
-			$scope.selected.topic = $db.model['topics'][0].id
-			unbind_topic_watch();
-		}
-	});
+  // set the location to the ghc 5 commons when it comes down
+  var stopLocationDefault = $scope.$watchCollection(function() {
+    return $db.model.locations;
+  }, function() {
+    var locs = $db.model.locations;
+    for (var i = 0; i < locs.length; i++) {
+      if (locs[i].location.toLowerCase().indexOf('common') !== -1) {
+        $scope.new_selected.location = locs[i].id.toString();
+        stopLocationDefault();
+      }
+    }
+  });
 
-	var unbind_location_watch = $scope.$watch(function () {
-		return $db.model['locations'].length;	
-	},function() {
-		if ($db.model['locations'].length > 0) {
-			$scope.selected.location = $db.model['locations'][0].id
-			unbind_location_watch();
-		}
-	});
+  // Add the question
+  $scope.ask_question = function() {
+    if (!$db.has_active_question() &&
+          $scope.new_selected.location !== '' &&
+          $scope.new_selected.topic !== '') {
+      $db.add_question({
+        "location_id": parseInt($scope.new_selected.location),
+        "topic_id": parseInt($scope.new_selected.topic),
+        "help_text": $scope.new_selected.help_text
+      });
+      $scope.new_selected.help_text = '';
+      $scope.new_selected.topic = '';
+    }
+  };
 
-	$scope.ask_question = function () {
-		//Check if question exists
-		if (($db.has_active_question())) {
-			$('#modalnewquestion').closeModal();
-			return
-		}
+  // Update the question and close the modal
+  $scope.update_question = function() {
+    $db.update_question({
+      "location_id": parseInt($scope.edit_selected.location),
+      "topic_id": parseInt($scope.edit_selected.topic),
+      "help_text": $scope.edit_selected.help_text
+    });
+    $('#modaleditquestion').closeModal();
+  };
 
-		//Add the question and close the modal
-		$db.add_question({
-			"location_id": parseFloat($scope.selected.location), 
-			"topic_id": parseFloat($scope.selected.topic), 
-			"help_text": $scope.selected.help_text
-		})
-		$('#modalnewquestion').closeModal();
-	}
-
-	$scope.update_question = function () {
-		//Add the question and close the modal
-		$db.update_question({
-			"location_id": parseFloat($scope.selected.location), 
-			"topic_id": parseFloat($scope.selected.topic), 
-			"help_text": $scope.selected.help_text
-		});
-		$('#modaleditquestion').closeModal();
-	}
+  // hack to force topics to correctly load
+  // angular materialize selects don't update correctly if there's a disabled
+  // element within the select. the code below circumvents this by forcing an
+  // update of the model backing the select, causing angular materialize to
+  // correctly refresh the select.
+  $scope.$watchCollection(function() {
+    return $db.model.topics;
+  }, function() {
+    $scope.new_selected.topic = null;
+    $scope.$evalAsync(function() {
+      $scope.new_selected.topic = '';
+    });
+  });
 
   $scope.getOffTime = function(question) {
     return new Date(question.off_time);
