e0616c0c2f0e224b830cfdfbf5a81e0e3c0de480
Edward Dryer <edryer@andrew.cmu.edu>
2017-01-14 19:21:21 -0500

    fix migrate and seed script now that we have alpine linux

--- deploy_configs/migrate_and_seed.sh
+++ deploy_configs/migrate_and_seed.sh
@@ -1,2 +1,2 @@
-docker exec 112_queue_nodeapp bash -c 'npm run-script migrate' 
-docker exec 112_queue_nodeapp bash -c 'npm run-script seed'
+#docker exec 112_queue_nodeapp ash -c 'npm run-script migrate' 
+docker exec 112_queue_nodeapp ash -c 'npm run-script seed'
