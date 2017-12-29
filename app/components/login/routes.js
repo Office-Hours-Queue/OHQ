96be94fe231c966bf3c6ebabb6949d403debe586
Edward Dryer <edryer@andrew.cmu.edu>
2017-12-28 22:17:46 -0500

    remove register/edit user routes

--- app/components/login/routes.js
+++ app/components/login/routes.js
@@ -2,10 +2,6 @@ var router = require('express').Router();
 var config = require('../../config');
 var auth = require('../../auth');
 
-router.post('/localauth',auth.passport.authenticate('local'),function(req, res) {
-  res.redirect('/#/' + req.user.role);
-});
-
 router.get('/googleauth',
   auth.passport.authenticate('google', { scope: ['openid profile email'] }));
 
